144c6ececb1333fc839e83257a42c9de611d8ff1
sushant-XD <105367904+sushant-XD@users.noreply.github.com>
2024-07-01 18:47:12 +0545

    Update boot.py

--- scripts/libraries/boot.py
+++ scripts/libraries/boot.py
@@ -3,6 +3,8 @@
 import sensor, image, time, machine
 
 RED_LED_PIN = 1
+BLUE_LED_PIN = 3
+GREEN_LED_PIN = 2
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
@@ -13,5 +15,15 @@ clock = time.clock()
 
 while(True):
     pyb.LED(RED_LED_PIN).on()
-    pyb.delay(1000);
+    pyb.delay(4000);
     pyb.LED(RED_LED_PIN).off()
+    pyb.delay(4000)
+    pyb.LED(BLUE_LED_PIN).on()
+    pyb.delay(4000);
+    pyb.LED(BLUE_LED_PIN).off()
+    pyb.delay(4000)
+    pyb.LED(GREEN_LED_PIN).on()
+    pyb.delay(4000)
+    pyb.LED(GREEN_LED_PIN).off()
+    pyb.delay(4000)
+    
